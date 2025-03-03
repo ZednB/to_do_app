2d50f403529475541fa4c2d0ec2bcc1919589c51
Zubayr Bibulatov <Ru.Zubayr@gmail.com>
2025-03-03 22:55:44 +0300

    Login view

--- users/urls.py
+++ users/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
 
 from users.apps import UsersConfig
-from users.views import home
+from users.views import home, UserCreate, UserLogin
 
 app_name = UsersConfig.name
 
 urlpatterns = [
     path('', home, name='base'),
+    path('create/', UserCreate.as_view(), name='create_user'),
+    # path('login/', LoginView.as_view(template_name='users/login_user.html'), name='login_user'),
+    path('login/', UserLogin.as_view(), name='login_user'),
 ]
--- users/views.py
+++ users/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import CreateView
+from django.urls import reverse_lazy
+from django.contrib.auth.views import LoginView
+
+from users.forms import UserForm
+from users.models import User
 
 
 def home(request):
     return render(request, 'users/base.html')
+
+
+class UserCreate(CreateView):
+    model = User
+    form_class = UserForm
+    template_name = 'users/create_user.html'
+    success_url = reverse_lazy('users:base')
+
+
+class UserLogin(LoginView):
+    template_name = 'users/login_user.html'
+    success_url = reverse_lazy('users:base')
+
